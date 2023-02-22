84ba8e95d479025a11f759f43e4916d05a39a637
unknown <nacho.morales5mail.com>
2023-02-22 16:03:37 -0300

    Henry dia 26

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,21 +2,25 @@ const process = require('process');
 const commands = require('./commands/index.js');
 
 let print = (output) => {
-	process.stdout.write(output);
-	process.stdout.write('\nprompt > ');
+	process.stdout.write(output); //Mostrar algo
+	process.stdout.write('\nprompt > '); //Vuelve a pedir el prompt
+	//stdout ===> para sacar datos de Node
 };
 
 let bash = () => {
 	process.stdout.write('prompt > ');
 
 	process.stdin.on('data', (data) => {
+		//stdin ===> ingresar datos en Node
 		let args = data.toString().trim().split(' ');
 
 		let cmd = args.shift();
 
 		commands[cmd]
-			? commands[cmd](print, args.join(' '))
+			? commands[cmd](print, args)
 			: print(`command not found: ${cmd}`);
+
+		// Tambien se puede con hasOwnProperty()
 	});
 };
 
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs'); //Files System
 const utils = require('../utils/request');
 const process = require('process');
 
@@ -11,42 +11,76 @@ let date = (print) => {
 };
 
 let echo = (print, args) => {
-	print(args);
+	print(args.join(' ')); //No pasa test, pero funciona
 };
 
 let ls = (print) => {
 	fs.readdir('.', (error, files) => {
-		if (error) throw Error(error);
-		print(files.join(' '));
+		if (error) throw new Error(error);
+		// print(files.join(' ')); // Asi pasa el test
+		print(files.join('\n')); // Asi es mejor
 	});
 };
 
 let cat = (print, args) => {
-	fs.readFile(args, 'utf-8', (error, data) => {
-		if (error) throw Error(error);
+	//Asi funciona, pero no pasa test
+	fs.readFile(args.join(' '), 'utf-8', (error, data) => {
+		if (error) throw new Error(error);
 		print(data);
 	});
 };
 
 let head = (print, args) => {
-	fs.readFile(args, 'utf-8', (error, data) => {
-		if (error) throw Error(error);
-		print(data.split('\n').at(0));
+	fs.readFile(args.join(' '), 'utf-8', (error, data) => {
+		if (error) throw new Error(error);
+		// print(data.split('\n').at(0)); // Pasa el test
+		print(data.split('\n').slice(0, 8).join('\n')); // Es correcto
 	});
 };
 
 let tail = (print, args) => {
-	fs.readFile(args, 'utf-8', (error, data) => {
-		if (error) throw Error(error);
+	//Asi funciona, pero no pasa test
+	fs.readFile(args.join(' '), 'utf-8', (error, data) => {
+		if (error) throw new Error(error);
 		print(data.split('\n').at(-1).trim());
 	});
 };
 
 let curl = (print, args) => {
-	utils.request(args, (error, response) => {
-		if (error) print(error.toString());
-		else print(response.toString());
-	});
+	if (args.toString().slice(0, 8) === 'https://') {
+		//Asi funciona, pero no pasa test
+		utils.request(args.toString(), (error, response) => {
+			// if (error) print(error.toString());
+			// else print(response.toString()); // Asi pasa el test
+			if (error) throw new Error(error);
+			print(response.data.toString());
+		});
+	} else {
+		print('Debe ingresar una URL https://');
+	}
+};
+
+let create = (print, args) => {
+	let componentName;
+	process.stdout.write('Ingresa el nombre del componente');
+	process.stdin.on('data', (data) => {});
+	fs.writeFile(
+		{ componentName } + '.jsx',
+		`
+	import React from "react" 
+	export default function ${componentName}(props){
+		return(
+			<div> 
+			Soy el componente nuevo 
+			</div>
+		)
+	}
+	`,
+		(error) => {
+			if (error) throw Error(error);
+			print('Componente creado satisfactoriamente');
+		},
+	);
 };
 
 module.exports = {
@@ -58,4 +92,5 @@ module.exports = {
 	head,
 	tail,
 	curl,
+	create,
 };
