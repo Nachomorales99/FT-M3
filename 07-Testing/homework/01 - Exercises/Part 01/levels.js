cb4b787e17c724580d5020d55f33379433eb803b
unknown <nacho.morales5mail.com>
2023-03-02 01:24:10 -0300

    Henry dia 32

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,31 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => a + b;
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+	if (letras.length < 2) return letras;
 
-const levelThree = (a, b) => {};
+	if (letras.length === 2) return letras[0];
 
-const levelFour = (num) => {};
+	if (letras.length > 2) {
+		let result = [];
+		for (let i = 0; i < letras.length; i++) {
+			if (i % 2 === 0) result.push(letras[i]);
+		}
+		return result.join('');
+	}
+};
+
+const levelThree = (a, b) => [...a, ...b].sort();
+
+const levelFour = (num) => {
+	let array = num.toString().split('');
+
+	let arrayNum = array.map((str) => Number(str));
+
+	let result = arrayNum.reduce((acc, num) => acc + num);
+
+	let reverse = Number(result.toString().split('').reverse().join(''));
+
+	return result * reverse === num;
+};
 
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.test.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.test.js
@@ -1,14 +1,24 @@
 const antiTrollsSecurity = require('./antiTrollsSecurity');
 
 describe('PARTE 02', () => {
-   describe('Seguridad Anti Trolls', () => {
-      it('Debe devolver el mismo string pero habiendo eliminado todas las vocales', () => {
-         expect(antiTrollsSecurity('This website is for losers LOL!')).toBe(
-            'Ths wbst s fr lsrs LL!'
-         );
-         expect(antiTrollsSecurity('What are you, a communist?')).toBe(
-            'Wht r y,  cmmnst?'
-         );
-      });
-   });
+	describe('Seguridad Anti Trolls', () => {
+		it('Debe devolver el mismo string pero habiendo eliminado todas las vocales', () => {
+			expect(antiTrollsSecurity('This website is for losers LOL!')).toBe(
+				'Ths wbst s fr lsrs LL!',
+			);
+			expect(antiTrollsSecurity('What are you, a communist?')).toBe(
+				'Wht r y,  cmmnst?',
+			);
+		});
+
+		it('Debe devolver el mismo string pero habiendo eliminado todas las vocales', () => {
+			expect(antiTrollsSecurity('Aguante el grupo 2')).toBe('gnt l grp 2');
+		});
+
+		it('Debe devolver el mismo string pero habiendo eliminado todas las vocales', () => {
+			expect(antiTrollsSecurity('Chat GPT, naaa Fede GPT')).toBe(
+				'Cht GPT, n Fd GPT',
+			);
+		});
+	});
 });
